8cab5ba71c98a49d9660e8d64d5719743ec23c69
telegram213540799849-ui <telegram213540799849@gmail.com>
2025-11-13 22:57:01 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,15 +1,13 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   try {
     const targetUrl = `http://129.151.230.246:9000${req.url}`;
     const response = await fetch(targetUrl);
-    const body = await response.arrayBuffer();
+    const buffer = await response.arrayBuffer();
 
     res.setHeader('Content-Type', response.headers.get('content-type') || 'application/octet-stream');
-    res.status(response.status).send(Buffer.from(body));
+    res.status(response.status).send(Buffer.from(buffer));
   } catch (err) {
+    console.error('Proxy error:', err);
     res.status(500).send('Proxy error');
-    console.error(err);
   }
 }
